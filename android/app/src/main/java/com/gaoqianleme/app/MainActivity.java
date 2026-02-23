489975d7095b2e49642fb73411a6a01dd37d5661
niklaus3016 <Jp182022@163.com>
2026-02-23 09:47:39 +0000

    fix: 移除对已删除BaiduAdPlugin的引用

--- android/app/src/main/java/com/gaoqianleme/app/MainActivity.java
+++ android/app/src/main/java/com/gaoqianleme/app/MainActivity.java
@@ -1,16 +1,11 @@
 package com.gaoqianleme.app;
 
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
-import com.baidu.union.BaiduAdPlugin;
 import android.os.Bundle;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        // 注册百度联盟广告插件
-        registerPlugin(BaiduAdPlugin.class);
     }
 }
